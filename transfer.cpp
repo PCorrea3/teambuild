9f2d1bc072bdc7141d3e0e3bc25d231bba779f7d
Unknown <pcorrea3@student.rcc.edu>
2017-03-23 15:34:41 -0700

    Edited cpp

--- transfer.cpp
+++ transfer.cpp
@@ -6,8 +6,6 @@ Transfer::Transfer(){
     title = new QLabel("Transfer Funds");
     QFont font = title->font();
     font.setPointSize(14);
-    QFont font2 = name->font();
-    font2.setPointSize(12);
 
     ok = new QPushButton("Ok");
     cancel = new QPushButton("Cancel");
@@ -15,7 +13,7 @@ Transfer::Transfer(){
     QGridLayout *layout = new QGridLayout();
     layout->setColumnStretch(1,1);
     layout->addWidget(title, 0,0);
-    layout->addWidget(submit, 1, 0);
+    layout->addWidget(ok, 1, 0);
     layout->addWidget(cancel, 1,1);
 
     setLayout(layout);
